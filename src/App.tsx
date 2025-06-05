5371992119ab9d065a7cc569d9792ef3eec91605
Jahanara Khatun <zarakhatun58@gmail.com>
2025-06-05 10:51:03 +0530

    update cors

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import Home from './pages/Home';
 
 function App() {
 const hostname = window.location.hostname; // e.g., shop1.localtest.me
-const parts = hostname.split('.');
-const isLocalhost = hostname.includes('localhost') || hostname.includes('127.0.0.1');
+// const parts = hostname.split('.');
+// const isLocalhost = hostname.includes('localhost') || hostname.includes('127.0.0.1');
 // const isSubdomain = parts.length > 2 || (!isLocalhost && parts.length > 1);
 const baseDomain = import.meta.env.VITE_BASE_DOMAIN || 'localhost';
 const isSubdomain = hostname.endsWith(baseDomain) && hostname.split('.').length > 2;
